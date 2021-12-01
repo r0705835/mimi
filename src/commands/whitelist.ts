fdb26c1a48fe3a9b6087aa60dc3ac947a8825c12
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 22:23:49 +0100

    create basic whitelist command

---
+++ src/commands/whitelist.ts
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInt } from "../interfaces/CommandInt";
+
+export const whitelist: CommandInt = {
+    data: new SlashCommandBuilder(),
+    run: async (interaction) => {
+
+    }
+}
