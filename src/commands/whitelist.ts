7381bb69527aca003b160929d90f28e33ce7aeef
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 22:48:11 +0100

    implement working version minecraft whitelist

--- src/commands/whitelist.ts
+++ src/commands/whitelist.ts
@@ -1,12 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { TextChannel } from "discord.js";
 import { CommandInt } from "../interfaces/CommandInt";
 
 
 export const whitelist: CommandInt = {
     data: new SlashCommandBuilder()
         .setName("whitelist")
+        .addStringOption(option =>
+            option.setName("username")
+                .setDescription("The username of your minecraft account.")
+                .setRequired(true))
         .setDescription("Whitelist yourself to the Atomic SMP minecraft server!"),
     run: async (interaction) => {
-        console.log("Not implemented yet!");
+        try {
+            const consoleChannel = await interaction.guild?.channels.fetch("910160166236880926")!;
+            (consoleChannel as TextChannel).send("whitelist add " + interaction.options.getString("username"));
+            interaction.reply({
+                content: "You have successfully been whitelisted to the minecraft server, enjoy your stay!",
+                ephemeral: true
+            })
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
