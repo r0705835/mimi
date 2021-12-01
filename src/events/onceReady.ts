94a05af2be5c435511b7d8949a1a9b248a3e6645
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 22:10:23 +0100

    register  slash commands automatically

--- src/events/onceReady.ts
+++ src/events/onceReady.ts
@@ -1,5 +1,15 @@
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
 import { Client } from "discord.js";
+import { CommandList } from "../commands/_CommandList";
+import { clientId, guildId, token } from "../config.json";
+
 
 export const onceReady = async (client: Client) => {
     console.log("Connected to Discord!");
+
+    const commands = CommandList.map(command => command.data.toJSON());
+    const rest = new REST({ version: '9' }).setToken(token);
+
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
 }
