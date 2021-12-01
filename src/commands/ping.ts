be1bc3e5c33a69e8fadee32c8cc99eb6a4987a66
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 22:10:35 +0100

    add name and description for ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,8 +2,10 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInt } from "../interfaces/CommandInt";
 
 export const ping: CommandInt = {
-    data: new SlashCommandBuilder(),
-    run: async (Interaction) => {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Sends pong back!"),
+    run: async (interaction) => {
         console.log("Not implemented yet!");
     }
 }
