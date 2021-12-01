4a79b16f5ba9cb0e685d9ffa5af241e0055c8f94
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 22:27:52 +0100

    make whitelist an executable command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,7 +1,10 @@
 import Collection from "@discordjs/collection";
 import { CommandInt } from "../interfaces/CommandInt";
 import { ping } from "./ping";
+import { whitelist } from "./whitelist";
 
 
 export const CommandList: Collection<string, CommandInt> = new Collection();
 CommandList.set(ping.data.name, ping);
+CommandList.set(whitelist.data.name, whitelist);
+
--- src/commands/whitelist.ts
+++ src/commands/whitelist.ts
@@ -1,9 +1,12 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInt } from "../interfaces/CommandInt";
 
-export const whitelist: CommandInt = {
-    data: new SlashCommandBuilder(),
-    run: async (interaction) => {
 
+export const whitelist: CommandInt = {
+    data: new SlashCommandBuilder()
+        .setName("whitelist")
+        .setDescription("Whitelist yourself to the Atomic SMP minecraft server!"),
+    run: async (interaction) => {
+        console.log("Not implemented yet!");
     }
 }
