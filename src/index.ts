4b49a72a2ee709e21b7a56feb0f35731a6680272
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-01 11:59:06 +0100

    create basic version of a discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Bonsoir Elliot!");
+import { Client, Intents } from "discord.js";
+import { token } from './config.json';
+
+
+(async () => {
+    const client = new Client({
+        intents: [Intents.FLAGS.GUILDS]
+    });
+    
+    client.once("ready", () => {
+        console.log("Connected to Discord!")
+    });
+    await client.login(token);
+})();
