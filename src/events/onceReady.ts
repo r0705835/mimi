35a42f552d708a7d07e8e26ed4f1a848b8b78fbf
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-05 12:58:27 +0100

    implement .env instead of config.json

--- src/events/onceReady.ts
+++ src/events/onceReady.ts
@@ -2,14 +2,14 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import { Client } from "discord.js";
 import { CommandList } from "../commands/_CommandList";
-import { clientId, guildId, token } from "../config.json";
+require("dotenv").config();
 
 
 export const onceReady = async (client: Client) => {
     console.log("Connected to Discord!");
 
     const commands = CommandList.map(command => command.data.toJSON());
-    const rest = new REST({ version: '9' }).setToken(token);
+    const rest = new REST({ version: '9' }).setToken(process.env.token!);
 
-    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+    rest.put(Routes.applicationGuildCommands(process.env.clientId!, process.env.guildId!), { body: commands });
 }
