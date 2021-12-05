ecb9b4a9c374f81a4ee5b470d781ac8a30fb02a0
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-05 17:55:00 +0100

    implement ExtendedClient

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import { Client, Intents } from "discord.js";
 import { onceReady } from "./events/onceReady";
 import { onInteractionCreate } from "./events/onInteractionCreate";
+import { ExtendedClient } from "./structures/Client";
 require("dotenv").config();
 
+const client = new ExtendedClient;
+client.start();
 (async () => {
-    const client = new Client({
-        intents: [Intents.FLAGS.GUILDS]
-    });
-    
     client.once("ready", (client) => onceReady(client));
     client.on("interactionCreate", async interaction => onInteractionCreate(interaction));
-    
-    await client.login(process.env.token);
 })();
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,12 +1,16 @@
 import { Client, Collection, Intents } from "discord.js";
 import { CommandInt } from "../interfaces/CommandInt";
 
-class ExtendedClient extends Client {
+export class ExtendedClient extends Client {
     commands: Collection<string, CommandInt> = new Collection();
 
     constructor() {
         super({
             intents: [Intents.FLAGS.GUILDS]
         });
+    }
+
+    async start() {
+        await this.login(process.env.token);
     }
 }
