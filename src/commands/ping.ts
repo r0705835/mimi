f65f1b4e0bdc319cbfb7a80149d65daf7eefc198
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-05 18:24:49 +0100

    update ping to ephemeral

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,14 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInt } from "../interfaces/CommandInt";
 
-
 export const ping: CommandInt = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Sends pong back!"),
     run: async (interaction) => {
-        interaction.reply("Pong!");
+        interaction.reply({
+            content: "Pong!",
+            ephemeral: true
+        });
     }
 }
