1519fdb012d9b2db28bfca0a948babd73cde1603
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-05 17:50:05 +0100

    create constructor of ExtendedClient

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,6 +1,12 @@
-import { Client, Collection } from "discord.js";
+import { Client, Collection, Intents } from "discord.js";
 import { CommandInt } from "../interfaces/CommandInt";
 
 class ExtendedClient extends Client {
     commands: Collection<string, CommandInt> = new Collection();
+
+    constructor() {
+        super({
+            intents: [Intents.FLAGS.GUILDS]
+        });
+    }
 }
