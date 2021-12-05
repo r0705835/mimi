1dcec0e7059b614fbb7d7223e5212b01f556f1f0
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-12-05 14:49:39 +0100

    create extended client

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,9 +1,25 @@
 import Collection from "@discordjs/collection";
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
+import { onceReady } from "../events/onceReady";
+import { onInteractionCreate } from "../events/onInteractionCreate";
 import { CommandInt } from "../interfaces/CommandInt";
 
 class ExtendedClient extends Client {
     public commands: Collection<string, CommandInt> = new Collection();
+
+    constructor() {
+        super({ intents: Intents.FLAGS.GUILDS });
+    }
+
+    start() {
+        this.events();
+        this.login(process.env.token);
+    }
+
+    async events() {
+        this.once("ready", (client) => onceReady(client));
+        this.on("interactionCreate", async interaction => onInteractionCreate(interaction));
+    }
 }
 
 export default ExtendedClient;
